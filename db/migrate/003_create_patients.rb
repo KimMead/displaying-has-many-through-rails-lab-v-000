110ad4d9e95ef6f61a17e75b6d7cbf134a570491
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-03-11 01:25:54 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_patients.rb
+++ db/migrate/003_create_patients.rb
@@ -1,1 +1,10 @@
-class CreatePatients < ActiveRecord::Migration 
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps null: false
+    end
+  end
+end
